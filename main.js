2d48cbab0f2f572a264588566920f4c27cdf282a
lloydpop <lloydperfect199@gmail.com>
2024-02-28 11:49:22 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const heroSection = document.querySelector(".hero");
+const navSection = document.querySelector(".nav-bar");
+window.addEventListener("load", () => {
+  heroSection.classList.add("active");
+  navSection.classList.add("active");
+});
